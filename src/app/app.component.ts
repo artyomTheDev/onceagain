ce2284719b167c4572778fc46699f61059e220af
EzeCrazy <123467580+EzeCrazy@users.noreply.github.com>
2025-12-16 23:38:53 +0300

    counter added & new comms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
+
+//i don't understand for what & how i use export class
 export class AppComponent {
   title = 'mentoring-first-project';
+count = 0;
 }
