6f2b9a6e285f228a289824979fc951118a27c11a
EzeCrazy <123467580+EzeCrazy@users.noreply.github.com>
2025-12-19 13:51:59 +0300

    refactor: replace hardcoded strings with template variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component} from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {NgIf} from "@angular/common";
+
+const time = new Date().getTime();
+
+console.log('time:', time);
+
+if (time === 21312314321432) {
+  console.log('time is correct')
+} else {
+  console.log('fooflo')
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -11,5 +23,20 @@ import {RouterOutlet} from '@angular/router';
 //i don't understand for what & how i use export class
 export class AppComponent {
   title = 'mentoring-first-project';
-count = 0;
+
+  isShowCatalog = true;
+
+  readonly headerItem1 = 'Главная';
+
+  readonly headerItem2 = 'О компании';
+
+  readonly headerItem3 = 'Каталог';
+
+  // showMessage(menuName) {
+  //
+  //   return menuName;
+  // }
+  //
+  // showMessage(headerItem2);
+
 }
