941349a06ab7c6a1d2a503fc0c0da60a7d01f77d
EzeCrazy <123467580+EzeCrazy@users.noreply.github.com>
2025-12-19 21:34:52 +0300

    feat: add ngFor cycle, complete 3/4 tasks of 6-th lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,28 @@
 import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 
-const time = new Date().getTime();
-
-console.log('time:', time);
-
-if (time === 21312314321432) {
-  console.log('time is correct')
-} else {
-  console.log('fooflo')
+for(let i:number = 0; i < 5; i++){
+  console.log(i)
 }
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf],
+  imports: [NgIf, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 
-//i don't understand for what & how i use export class
 export class AppComponent {
   title = 'mentoring-first-project';
 
-  isShowCatalog = true;
+  isShowCatalog:boolean = true;
 
-  isShowBanner = true;
+  isShowBanner:boolean = true;
 
-  readonly aboutCompany = this.showMessage('О компании')
+  readonly newPages:number[] = [5, 4, 3, 2, 1];
+
+  readonly aboutCompany:string = this.showMessage('О компании')
 
   readonly headerItem1:string = 'Главная';
 
